39cfef46e8825e32a568aaa348c0244c0005e8c0
selenayy-h <selenay_hur@icloud.com>
2024-12-19 16:31:02 +0300

    değişiklik görünüyor hala

---
+++ Hairr/Controllers/apiservisi.cs
@@ -0,0 +1,42 @@
+﻿//using Hairr.Services;
+//using Microsoft.AspNetCore.Authorization;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
+//using System.IO;
+//using System.Threading.Tasks;
+
+//namespace Hairr.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    [Authorize(Roles = "User,Admin")] // Kullanıcı ve Admin rolündeki kullanıcılar erişebilir
+//    public class AIController : ControllerBase
+//    {
+//        private readonly AIService _aiService;
+
+//        public AIController(AIService aiService)
+//        {
+//            _aiService = aiService;
+//        }
+
+//        // POST: api/AI/AnalyzeHair
+//        [HttpPost("AnalyzeHair")]
+//        public async Task<IActionResult> AnalyzeHair(IFormFile image)
+//        {
+//            if (image == null || image.Length == 0)
+//            {
+//                return BadRequest("Lütfen geçerli bir resim yükleyin.");
+//            }
+
+//            using (var ms = new MemoryStream())
+//            {
+//                await image.CopyToAsync(ms);
+//                var imageData = ms.ToArray();
+
+//                var analysisResult = await _aiService.AnalyzeHairStyleAsync(imageData);
+
+//                return Ok(new { Suggestion = analysisResult });
+//            }
+//        }
+//    }
+//}
---
+++ Hairr/Migrations/20241218103018_AddPersonelUygunlukGun.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hairr.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPersonelUygunlukGun : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UygunlukGunler",
+                table: "Personels");
+
+            migrationBuilder.CreateTable(
+                name: "PersonelUygunlukGunler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Gun = table.Column<int>(type: "int", nullable: false),
+                    PersonelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonelUygunlukGunler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonelUygunlukGunler_Personels_PersonelId",
+                        column: x => x.PersonelId,
+                        principalTable: "Personels",
+                        principalColumn: "PersonelId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonelUygunlukGunler_PersonelId",
+                table: "PersonelUygunlukGunler",
+                column: "PersonelId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonelUygunlukGunler");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UygunlukGunler",
+                table: "Personels",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
